eb49d64ff2b9eb13d4cc700ce48808a502d74cfc
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2020-02-29 18:04:11 +0100

    Update Notification.java

--- src/main/java/traynotification/notification/Notification.java
+++ src/main/java/traynotification/notification/Notification.java
@@ -2,7 +2,7 @@ package traynotification.notification;
 
 public enum Notification{
   INFORMATION("images/info.png", "#2C54AB"),
-  NOTICE("images/notice.png", "#8D9695"),
+  NOTICE("images/notice.png", "#f6e606"),
   SUCCESS("images/success.png", "#009961"),
   WARNING("images/warning.png", "#E23E0A"),
   ERROR("images/error.png", "#CC0033");
